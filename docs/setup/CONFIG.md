b991858907c3dcce77ce4892505e90334ec21132
adammotzel <adam.motzel@gmail.com>
2026-08-01 14:07:02 -0500

    update config

--- docs/setup/CONFIG.md
+++ docs/setup/CONFIG.md
@@ -7,6 +7,11 @@ The app requires the following environment variables:
 1. `NAME`: My pet's name, injected into the HTML templates for display.
 2. `POSTGRES_PW`: The app's Postgres user password.
 3. `NETWORK_NAME`: Name of the Wi-Fi network the app is running on, attributed to every prediction logged during the run.
+4. `DB_HOST`: Postrgres host. Usually "localhost".
+5. `DB_PORT`: Postgres host port. Usually 5432.
+6. `DB_USER`: App's database username. For this project, "photoapp_user".
+7. `DB_NAME`: App database name. For this project, "photoapp".
+8. `TEST_DB_NAME`: App's test database name. For this project, "photoapp_test".
 
 I store these in a `.env` file (in `src`).
 
--- src/config.py
+++ src/config.py
@@ -6,20 +6,19 @@ from fastapi.templating import Jinja2Templates
 
 # valid creds
 load_dotenv(Path(__file__).resolve().parent / ".env")
-NAME = os.getenv("NAME", "My Dog")
+NAME = os.environ["NAME"]
 NETWORK_NAME = os.environ["NETWORK_NAME"]
 POSTGRES_APP_PW = os.environ["POSTGRES_PW"]
 
 # DB config
-DB_HOST = "localhost"
-DB_PORT = "5432"
+DB_HOST = os.environ["DB_HOST"]
+DB_PORT = os.environ["DB_PORT"]
+DB_USER = os.environ["DB_USER"]
 
-if os.getenv("ENVIRONMENT") == "test":
-    DB_NAME = "photoapp_test"
+if os.getenv("TEST_ENV"):
+    DB_NAME = os.environ["TEST_DB_NAME"]
 else:
-    DB_NAME = "photoapp"
-
-DB_USER = "photoapp_user"
+    DB_NAME = os.environ["DB_NAME"]
 
 # file serving
 templates = Jinja2Templates(directory="src/templates")
